8df3282dbdfc52db66f0ddf4059450ffcfa09ad4
Ping Chiang <spclight@gmail.com>
2015-10-16 16:59:33 +0800

    Add: statistics controller(incomplete)

---
+++ app/controllers/statistics_controller.rb
@@ -0,0 +1,8 @@
+class StatisticsController < ApplicationController
+  def show
+    @employees = Employee.all
+    @time_logs = @employee.time_logs
+    @hrs_logged_overall = {}
+    @hrs_logged = @time_logs.map{|tl| [tl.date_time_in, TimeDifference.between(tl.date_time_in, tl.date_time_out).in_hours]}
+  end
+end
