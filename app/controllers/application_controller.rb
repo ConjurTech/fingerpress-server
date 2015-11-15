cdb55aba7b5604378737758774dcd1d321c71708
henrychuahb-mac <henrychuahb@gmail.com>
2015-11-15 23:04:00 +0800

    Change app to accommodate api keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,43 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  # This is our new function that comes before Devise's one
+  before_filter :authenticate
+
+  private
+
+  def authenticate_admin_from_token!
+    admin_email = request.headers["HTTP_ADMIN_EMAIL"].presence
+    admin = admin_email && Admin.find_by_email(admin_email)
+    # Notice how we use Devise.secure_compare to compare the token
+    # in the database with the token given in the params, mitigating
+    # timing attacks.
+    if admin && Devise.secure_compare(admin.authentication_token, request.headers["HTTP_AUTHENTICATION_TOKEN"])
+      sign_in admin, store: false
+    else
+      false
+    end
+  end
+
+  def authenticate
+    admin_signed_in? || authenticate_admin_from_token! || render_unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      User.find_by(auth_token: token)
+    end
+  end
+
+  def render_unauthorized
+    self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+    if request.env['PATH_INFO'] != new_admin_session_path # to avoid redirect loop
+      respond_to do |format|
+        format.html { redirect_to new_admin_session_path, notice: 'Please sign in' }
+        format.json { render json: 'Bad credentials', status: 401 }
+      end
+    end
+  end
 end
