352e7439cb082fe64bea28f195e5099ea9386bd6
henrychuahb-mac <henrychuahb@gmail.com>
2016-01-02 14:47:38 +0800

    Resolve issue where PayScheme was unable to be deleted

--- app/models/pay_scheme.rb
+++ app/models/pay_scheme.rb
@@ -3,6 +3,7 @@ class PayScheme < ActiveRecord::Base
   belongs_to :ot_type
   belongs_to :public_holiday_type
   belongs_to :weekend_type
+  has_many :employees, dependent: :nullify
   validates :name, :pay_type, :pay, presence: true
 
   def hourly_type?
