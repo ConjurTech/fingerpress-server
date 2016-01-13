a46ef8fba8d43b467341ea57c7db347f7f3d65b6
henrychuahb-mac <henrychuahb@gmail.com>
2016-01-14 01:10:13 +0800

    Uncomment business time

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -7,28 +7,28 @@
 
 
 
-# BusinessTime::Config.work_week = Workday.find_work_days
-# #BusinessTime::Config.work_week = ['Mon']
-#
-# @work_hours_list = {}
-# Workday.find_work_days.each do |wd|
-#   if wd == "Mon"
-#     @work_hours_list[:mon]=[Workday.find(1).twentyfour_hr_start_time, Workday.find(1).twentyfour_hr_end_time]
-#   elsif wd == "Tue"
-#     @work_hours_list[:tue]=[Workday.find(2).twentyfour_hr_start_time, Workday.find(2).twentyfour_hr_end_time]
-#   elsif wd == "Wed"
-#     @work_hours_list[:wed]=[Workday.find(3).twentyfour_hr_start_time, Workday.find(3).twentyfour_hr_end_time]
-#   elsif wd == "Thu"
-#     @work_hours_list[:thu]=[Workday.find(4).twentyfour_hr_start_time, Workday.find(4).twentyfour_hr_end_time]
-#   elsif wd == "Fri"
-#     @work_hours_list[:fri]=[Workday.find(5).twentyfour_hr_start_time, Workday.find(5).twentyfour_hr_end_time]
-#   elsif wd == "Sat"
-#     @work_hours_list[:sat]=[Workday.find(6).twentyfour_hr_start_time, Workday.find(6).twentyfour_hr_end_time]
-#   elsif wd == "Sun"
-#     @work_hours_list[:sun]=[Workday.find(7).twentyfour_hr_start_time, Workday.find(7).twentyfour_hr_end_time]
-#   end
-# end
-# BusinessTime::Config.work_hours = @work_hours_list
+BusinessTime::Config.work_week = Workday.find_work_days
+#BusinessTime::Config.work_week = ['Mon']
+
+@work_hours_list = {}
+Workday.find_work_days.each do |wd|
+  if wd == "Mon"
+    @work_hours_list[:mon]=[Workday.find(1).twentyfour_hr_start_time, Workday.find(1).twentyfour_hr_end_time]
+  elsif wd == "Tue"
+    @work_hours_list[:tue]=[Workday.find(2).twentyfour_hr_start_time, Workday.find(2).twentyfour_hr_end_time]
+  elsif wd == "Wed"
+    @work_hours_list[:wed]=[Workday.find(3).twentyfour_hr_start_time, Workday.find(3).twentyfour_hr_end_time]
+  elsif wd == "Thu"
+    @work_hours_list[:thu]=[Workday.find(4).twentyfour_hr_start_time, Workday.find(4).twentyfour_hr_end_time]
+  elsif wd == "Fri"
+    @work_hours_list[:fri]=[Workday.find(5).twentyfour_hr_start_time, Workday.find(5).twentyfour_hr_end_time]
+  elsif wd == "Sat"
+    @work_hours_list[:sat]=[Workday.find(6).twentyfour_hr_start_time, Workday.find(6).twentyfour_hr_end_time]
+  elsif wd == "Sun"
+    @work_hours_list[:sun]=[Workday.find(7).twentyfour_hr_start_time, Workday.find(7).twentyfour_hr_end_time]
+  end
+end
+BusinessTime::Config.work_hours = @work_hours_list
 
 
 
